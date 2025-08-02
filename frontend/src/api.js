b4d3fde0f149fb613f782e6de434a23abd83fee0
Nguyen Duc Chi <nguyenchi23324@gmail.com>
2025-08-02 10:04:59 +0700

    chinhapi

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,4 +1,7 @@
 import axios from 'axios';
-const API = axios.create({ baseURL: 'http://localhost:5000/api' });
+
+const API = axios.create({
+  baseURL: 'http://localhost:5000/api',  // ✅ thêm /api ở đây
+});
 
 export default API;
